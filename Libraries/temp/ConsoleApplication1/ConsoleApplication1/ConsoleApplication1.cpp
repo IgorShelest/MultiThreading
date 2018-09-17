ee488cb85d80b93a609bc72ed0fb685f8744f66a
Igor Shelest <igor.shelest@eleks.com>
2018-09-17 18:53:55 +0300

    Deleted test application

--- Libraries/temp/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9d19bd50e1417b048d76033e553327ba74a07d4bae47a2cd2b1ff633f6492405
-size 1722
--- Libraries/temp/ConsoleApplication1/ConsoleApplication1/pch.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6f4b5329e820609866192d2b58228d93b35d321f2a8dd4911c7ef8b4d5f5e5c6
-size 1262
